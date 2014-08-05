a1010c8cb9d70374c273e4f758ccf5c634cbe008
dnlms <daniel@dadt022ab.dcinfotec.local>
2014-08-04 19:19:56 -0500

    Cambios a SQL
    Cambios a la consulta inicial y se agregan noticias

--- externallib.php
+++ externallib.php
@@ -21,6 +21,11 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 require_once($CFG->libdir . "/externallib.php");
+/*require_once($CFG->libdir . "/datalib.php");
+require_once $CFG->libdir.'/gradelib.php';
+require_once $CFG->dirroot.'/grade/lib.php';
+require_once $CFG->dirroot.'/grade/report/grader/lib.php';*/
+
 
 class local_wsgradebook_external extends external_api {
 
@@ -54,32 +59,60 @@ class local_wsgradebook_external extends external_api {
      */
     public static function get_gradebook() {
         global $CFG, $DB, $USER;
+
+        /*$ccc = get_courses('all', 'c.id ASC', 'c.id,c.shortname,c.visible');
+
+        foreach ($ccc as $course) {
+            $context = context_course::instance($course->id, IGNORE_MISSING);
+            $gpr = new grade_plugin_return(array('type'=>'report', 'plugin'=>'grader', 'courseid'=>$course->id, 'page'=> 0));
+            $report = new grade_report_grader($course->id, $gpr, $context, 0, 0);
+            // final grades MUST be loaded after the processing
+            $report->load_users();
+            $numusers = $report->get_numusers();
+            echo "Course ID:".$course->id. " Usuarios: ".$numusers."\n";
+            $report->load_final_grades();
+            if($numusers > 0){
+                //print_r($report->users);
+                //print_r($report->gtree->items);
+                //print_r($report->grades);
+            }
+        }*/
+
+
         $gradebooks = array();
-        $gb_sql = "SELECT cu.idnumber AS course_id,
-                    cu.id AS course_idnum,
+        $gb_sql = "SELECT gg.id AS gid, 
+                    cu.id AS course_id,
                     usr.id as student_id,
-                    gi.itemname AS gradebookitem,
-                    gg.rawgrade AS raw_grade,
-                    gg.finalgrade AS final_grade,
+                    gi.itemname AS itemname,
+                    gi.itemtype AS itemtype,
+                    gi.itemmodule AS itemmodule,
+                    gg.rawgrade AS rawgrade,
+                    gg.finalgrade AS finalgrade,
+                    gg.usermodified AS usermodified,
                     gg.timecreated AS date_created,
                     gg.timemodified AS date_modified
                     FROM {grade_grades} AS gg
-                    LEFT JOIN {user} AS usr ON gg.userid = usr.id
-                    LEFT JOIN {grade_items} AS gi ON gg.itemid = gi.id
-                    LEFT JOIN {course} AS cu ON gi.courseid = cu.id";
-        $gbrecords = $DB->get_records_sql($gb_sql)
+                    JOIN {user} AS usr ON gg.userid = usr.id
+                    JOIN {grade_items} AS gi ON gg.itemid = gi.id
+                    JOIN {course} AS cu ON gi.courseid = cu.id 
+                    ORDER BY course_id, student_id";
+        $gbrecords = $DB->get_records_sql($gb_sql, $params=null, $limitfrom=0, $limitnum=0);
+
         if ($gbrecords) {
             foreach ($gbrecords as $gb) {
-                $gradebooks[] = array(
+                array_push($gradebooks, array(
                     'id' => $gb->course_id,
-                    'id_curso' => $gb->course_idnum,
+                    'id_curso' => $gb->course_id,
                     'id_alumno' => $gb->student_id,
-                    'item' => $gb->gradebookitem,
-                    'raw' => $gb->raw_grade,
-                    'final' => $gb->final_grade,
+                    'itemname' => $gb->itemname,
+                    'itemtype' => $gb->itemtype,
+                    'itemmodule' => $gb->itemmodule,
+                    'rawgrade' => $gb->rawgrade,
+                    'finalgrade' => $gb->finalgrade,
+                    'usermodified' => $gb->usermodified,
                     'datecreated' => $gb->date_created,
                     'datemodified' => $gb->date_modified
-                );
+                ));
             }
 
         }
@@ -99,9 +132,12 @@ class local_wsgradebook_external extends external_api {
                     'id' => new external_value(PARAM_RAW, 'course id number'),
                     'id_curso' => new external_value(PARAM_INT, 'id of course'),
                     'id_alumno' => new external_value(PARAM_INT, 'id of student'),
-                    'item' => new external_value(PARAM_RAW, 'concept of grade'),
-                    'raw' => new external_value(PARAM_RAW, 'raw calification of item'),
-                    'final' => new external_value(PARAM_RAW, 'final calification of item'),
+                    'itemname' => new external_value(PARAM_RAW, 'concept of grade'),
+                    'itemtype' => new external_value(PARAM_RAW, 'concept of grade'),
+                    'itemmodule' => new external_value(PARAM_RAW, 'Module of grade'),
+                    'rawgrade' => new external_value(PARAM_RAW, 'raw calification of item'),
+                    'finalgrade' => new external_value(PARAM_RAW, 'final calification of item'),
+                    'usermodified' => new external_value(PARAM_INT, 'User of the modification to item'),
                     'datecreated' => new external_value(PARAM_INT, 'the item date of creation'),
                     'datemodified' => new external_value(PARAM_INT, 'the item date of modification'),
                 )
